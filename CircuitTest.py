993220eab9f6fec03a0427f06da1bbffb2619417
Unknown <suvambasak2008@gmail.com>
2018-02-22 16:09:06 +0530

    All Test Option

--- CircuitTest.py
+++ CircuitTest.py
@@ -18,22 +18,30 @@ print ('---------------------------')
 print ('Press Enter to continue..')
 input()
 
-## LED Test function.
+# LED Test function.
 def test_LED():
-	print ('------------LED Test--------------')
+	print ('------------ LED Test --------------')
+
 	GPIO.setup(LED,GPIO.OUT)
+
 	print ("LED ON")
 	GPIO.output(LED,GPIO.HIGH)
+
 	for i in range(0,3):
+		print (i,'second')
 		time.sleep(1)
+
 	print ("LED OFF")
 	GPIO.output(LED,GPIO.LOW)
 
-## Button Test function.
+
+# Button Test function.
 def test_button():
-	print ('----------Button Test-----------')
+	print ('---------- Button Test -----------')
+
 	GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	push = 0
+
 	try:
 		while push < 3:
 			if GPIO.input(26) == True:
@@ -43,30 +51,53 @@ def test_button():
 	except Exception as e:
 		print ('[*] Exception ::'+str(e))
 
-## Distance Test function.
+
+# Distance Test function.
 def test_distance():
-	print ('---------Distance Test-------')
+	print ('--------- Distance Test -------')
+
 	GPIO.setup(TRIG, GPIO.OUT)
 	GPIO.setup(ECHO, GPIO.IN)
+
 	GPIO.output(TRIG, True)
 	time.sleep(0.00001)
 	GPIO.output(TRIG, False)
+
 	while GPIO.input(ECHO) == False:
 		start = time.time()
 	while GPIO.input(ECHO) == True:
 		end = time.time()
+
 	try:
 		sig_time = end-start
+
 		distance = sig_time / 0.000058
 		print ('\nDistance : {} cm'.format(distance))
+
 		distance = sig_time / 0.000148
 		print ('Distance : {} inchs'.format(distance))
-
 	except Exception as e:
 		print ('[**] Exception :: '+str(e))
 
 try:
-	test_distance()
+	req = input('Test All Componemts (y/n) : ')
+	if req == 'y' or req == 'Y':
+		print ('\n'*3)
+		print ('Starting All Test.')
+
+		print ('\n'*3)
+		test_LED()
+
+		print('\n' * 3)
+		test_button()
+
+		print('\n' * 3)
+		test_distance()
+	elif req == 'n' or req == 'N':
+		pass
+	else:
+		print ('Exit.')
+
 except Exception as e:
 	print ('[*] Exception :: '+str(e))
 finally:

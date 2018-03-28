83c5def96a592fabb8e6ab3fec11480b44a40922
suvambasak <suvambasak2008@gmail.com>
2018-03-29 00:49:50 +0530

    Export IP address

---
+++ LockDevice/Export_IP.py
@@ -0,0 +1,15 @@
+import subprocess as sp
+import urllib.request
+
+
+op = sp.check_output("""ifconfig wifi0|grep "inet "|awk -F'[: ]+' '{ print $4 }'""",shell=True)
+
+sendtext = op.decode().split()[0]
+# print (sendtext)
+
+
+try:
+    resp = urllib.request.urlopen('https://techcodebox.000webhostapp.com/lock/text.php?info='+sendtext)
+    print (resp.read().decode())
+except Exception as e:
+    print (str(e))
